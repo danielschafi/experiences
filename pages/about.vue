0b01c6878caf4e9915609643e75524d2bca13084
danielschafi <daniel.schafi@bluewin.ch>
2024-06-21 01:13:18 +0200

    contact & about

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    about me
+  <div class="flex items-center justify-center min-h-full bg-lightgray">
+    <div class="w-full max-w-4xl p-8 min-h-screen bg-white shadow-md">
+      <h1 class="mb-6 text-2xl font-bold text-emerald">Welcome to XPTogether: Experience Life Together! </h1>        
+      <p>
+        Are you ready to dive into extraordinary adventures? <br>At XPTogether, we connect you with like-minded individuals eager to share unforgettable experiences. Discover activities ranging from hiking trips and cooking classes to game nights and book clubs, everything you can imagine. <br>
+        Have a passion to share? Create and host your own events! Meet new friends, expand your social circle, and embrace the excitement of spontaneous adventures. Our community celebrates diversity, inclusivity, and the joy of living life to the fullest. <br>
+        Start your journey with XPTogether, where every moment is an opportunity to Experience Together!
+      </p>
+        
+    </div>
   </div>
 </template>
 
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
-    <h2>Contact Data</h2>
-    <p>PLEASE DO NOT CONTACT</p>
-    Schafhäutle Daniel
-    +41 79 157 76 75
-    daniel.schafhaeutle@stud.fhgr.ch
+  <div class="flex items-center justify-center min-h-full bg-lightgray">
+    <div class="w-full max-w-md p-8 min-h-screen bg-white shadow-md">
+      <h1 class="mb-6 text-2xl font-bold text-emerald">Contact</h1>        
+        <div class="block">
+          <label>Name: </label>Schafhäutle Daniel
+        </div>
+        <div class="block">
+          <label>Email: </label>daniel.schafhaeutle@stud.fhgr.ch
+        </div>
+        <div class="block">
+          <label>Phone: </label> +41 79 157 76 75
+        </div>
+        <div class="  text-black italic mt-2">
+          *PLEASE DO NOT CONTACT
+        </div>
+        
+    </div>
   </div>
 </template>
 
