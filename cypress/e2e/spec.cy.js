9401999dd65c66c3e7b36f3804a3b4d8086e5c54
danielschafi <daniel.schafi@bluewin.ch>
2024-06-21 01:44:50 +0200

    added a cypress test. pretty nice actually

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
+describe('Test Login Redirect to landing page', () => {
   it('passes', () => {
-    cy.visit("http://localhost:3000/")
+    cy.visit("/")
+    cy.clearAllCookies()
+    cy.clearAllSessionStorage()
+    cy.get('[href="/profile"]').click()
+    cy.get('#email').type("aragornow@gmail.com");
+
+    cy.get('#password').type("Test123")
+
+
+    cy.get('.space-y-4 > .text-white').click()
+    cy.url().should('eq', Cypress.config('baseUrl') + '/');
+  
+  
   })
+
 })
