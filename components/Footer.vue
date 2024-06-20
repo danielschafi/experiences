dfc498662d416f7b1275093e16bcee9e8dda032a
danielschafi <daniel.schafi@bluewin.ch>
2024-06-21 01:14:46 +0200

    adjusted footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
   <footer class="relative bottom-0 left-0 w-full py-6 text-white bg-eucalyptus">
-    <div class="container px-4 ml-4 mr-auto">
+    <div class="container px-4 ml-2 mr-auto">
       <div class="flex flex-row items-center justify-between">
         <div >
           <ul>
             <li class="my-2">
-            <NuxtLink class="text-sm text-center hover:text-pottery"  to="/contact">Contact</NuxtLink>
+            <NuxtLink class="text-sm text-center hover:text-emerald"  to="/contact">Contact</NuxtLink>
             </li>
             <li class="my-2">
-            <NuxtLink class="text-sm text-center hover:text-pottery"  to="/about">About</NuxtLink>
+            <NuxtLink class="text-sm text-center hover:text-emerald"  to="/about">About</NuxtLink>
             </li>
             <li class="my-2"><p class="text-sm">&copy; 2024 XPTogether. All rights reserved.</p></li>
             
